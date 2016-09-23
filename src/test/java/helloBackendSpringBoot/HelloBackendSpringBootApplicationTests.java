60b8bf97b926d1823319894c571990fbe05bef4a
Artem Dvornichenko <artem.dvornichenko@gmail.com>
2016-09-23 14:38:15 +0300

    commit 5

--- src/test/java/helloBackendSpringBoot/HelloBackendSpringBootApplicationTests.java
+++ src/test/java/helloBackendSpringBoot/HelloBackendSpringBootApplicationTests.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import helloBackendSpringBoot.domain.Contact;
@@ -20,6 +21,7 @@ import helloBackendSpringBoot.service.ContactService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class HelloBackendSpringBootApplicationTests {
 	
 	@Autowired
